b9f7984542e92aa8691eeb1df5f1bfb308bc4cf8
vikie1 <39300268+vikie1@users.noreply.github.com>
2021-09-06 12:17:17 +0300

    initialisng the project

---
+++ src/main/java/io/github/vikie1/whatsoftware/WhatSoftwareApplication.java
@@ -0,0 +1,13 @@
+package io.github.vikie1.whatsoftware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatSoftwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatSoftwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/vikie1/whatsoftware/WhatSoftwareApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.vikie1.whatsoftware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatSoftwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
